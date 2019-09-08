b0940550cbc8b13095860d4ef57c3e667cff41bf
Retro <mossiat.jeoffrey@outlook.com>
2019-09-08 18:08:17 +0200

    Set : Agular - Netifly

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    AboutComponent
+    HomeComponent
   ],
   imports: [
     BrowserModule,
