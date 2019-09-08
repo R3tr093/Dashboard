cee63338aa17f67a417982cc49e7e80466fa7501
Retro <mossiat.jeoffrey@outlook.com>
2019-09-08 18:29:04 +0200

    Add : bootstrap && components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { HeadComponent } from './head/head.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    HeadComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
