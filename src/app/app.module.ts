012c690785410394f2c4ce3099622b12336cd52d
Retro <mossiat.jeoffrey@outlook.com>
2019-09-09 09:40:39 +0200

    Add: Clock in head constructor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { HeadComponent } from './head/head.component';
 import { FooterComponent } from './footer/footer.component';
+import { mainService } from './services/app.service';
 
 
 @NgModule({
@@ -19,7 +20,7 @@ import { FooterComponent } from './footer/footer.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [mainService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/head/head.component.ts
+++ src/app/head/head.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import { mainService } from '../services/app.service';
 
 @Component({
   selector: 'app-head',
@@ -7,9 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeadComponent implements OnInit {
 
-  constructor() { }
+  Clock: object = new Date();
+
+
+  constructor(private mainService: mainService) {
+
+    setInterval(
+      () => {
+        this.Clock = new Date;
+      }, 1000
+    );
+    
+
+   }
 
   ngOnInit() {
+  }
+
+  refreshClock()
+  {
+    this.Clock = new Date;
+    
   }
 
 }
