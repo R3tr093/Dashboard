f8b70e0eadf7646c23b7da25cf25934df01ed241
root <root@localhost.localdomain>
2019-09-15 18:03:31 +0200

    Add: Prepare toogle modal on space bar event

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,3 +8,37 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Dashboard';
 }
+
+document.body.onkeyup = function(e){
+
+
+
+   if(e.keyCode === 32)
+   {
+
+    let element = document.getElementById('cmd');
+    let val = element.style.display;
+
+    console.log(val)
+
+    if(val === "" || val === "none")
+    {
+      element.style.display = "block";
+    }
+
+    else
+    {
+      element.style.display = "none";
+    }
+
+   }
+
+   else
+  {
+    alert('nope')
+  }
+
+  
+
+   
+}
