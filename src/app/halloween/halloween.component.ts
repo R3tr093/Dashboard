8273ef88e973a78e91557c6e59dea77f7d2d12b3
Retro <mossiat.jeoffrey@outlook.com>
2019-09-21 19:06:37 +0200

    Add : new feature portal halloween setup

--- src/app/halloween/halloween.component.ts
+++ src/app/halloween/halloween.component.ts
@@ -11,12 +11,51 @@ export class HalloweenComponent implements OnInit {
   Quotes : any[];
   Jokes : any[];
   Score : any;
+  Boxs = [
+    {
+      id : "1",
+      title: "Un titre 1",
+      link: "Un lien 1"
+    },
+    {
+      id:  "2",
+      title: "Un titre 2",
+      link: "Un lien 2"
+    },
+    {
+      id: "3",
+      title: "Un titre 3",
+      link: " Un lien 3"
+    },
+    {
+      id: "4",
+      title: "Un titre 4",
+      link: "Un lien 4"
+    },
+    {
+      id: "5",
+      title: "Un titre 5",
+      link: "Un lien 5"
+    },
+    {
+      id: "6",
+      title: "Un titre 6",
+      link: " Un lien 6"
+    }
+    
+
+  ]
+  
+
+ 
 
   constructor(private appService: appService) { 
     this.Quotes = appService.Quotes;
     this.Jokes = appService.Jokes;
     this.Score = appService.Score;
   }
+
+  
 
   ngOnInit() {
   
